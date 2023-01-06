600fe4b992d5728d5fe4094c56493e4ea2083eb8
Tanmoy <76746527+tanmoyM-1611@users.noreply.github.com>
2023-01-06 16:44:07 +0600

    Update cgpaCount.pl

--- cgpaCount.pl
+++ cgpaCount.pl
@@ -1,11 +1,11 @@
-name(shakil,gpa(3.65,university(comilla_university))).
-name(mahfuz,gpa(3.49,university(comilla_university))).
-name(shanto,gpa(3.49,university(comilla_university))).
-name(mandol,gpa(3.49,university(comilla_university))).
-name(sifat,gpa(3.51,university(comilla_university))).
-name(moin,gpa(3.49,university(comilla_university))).
-name(samrat,gpa(3.70,university(comilla_university))).
-name(majhar,gpa(3.63,university(comilla_university))).
-name(zilani,gpa(3.69,university(comilla_university))).
-name(mamun,gpa(3.23,university(comilla_university))).
+name(shakil,gpa4.65,university(comilla_university))).
+name(mahfuz,gpa(4.49,university(comilla_university))).
+name(shanto,gpa(4.49,university(comilla_university))).
+name(mandol,gpa(7.49,university(comilla_university))).
+name(sifat,gpa(7.51,university(comilla_university))).
+name(moin,gpa(2.49,university(comilla_university))).
+name(samrat,gpa(35.70,university(comilla_university))).
+name(majhar,gpa(39.63,university(comilla_university))).
+name(zilani,gpa(34.69,university(comilla_university))).
+name(mamun,gpa(35.23,university(comilla_university))).
 //demo result
